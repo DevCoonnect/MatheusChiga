063aa277bbcd8e7b1ab2066f9ed89ca2856fb380
Matheus Chiga <matheuschiga1303@gmail.com>
2023-11-16 20:45:44 -0300

    Alteração para diminuir linhas
    Alteração para diminuir linhas

--- ProjetoPloomesMatheusChiga/Program.cs
+++ ProjetoPloomesMatheusChiga/Program.cs
@@ -7,10 +7,7 @@ class Program
         Treino treino1 = new Treino("Treino A", "Força e resistência");
 
         Exercicio exercicio1 = new Exercicio { Nome = "Supino Reto", Descricao = "Exercício para peitorais" };
-        Exercicio exercicio2 = new Exercicio { Nome = "Supino Inclinado", Descricao = "Exercício para peitorais" };
-
         treino1.AdicionarExercicio(exercicio1);
-        treino1.AdicionarExercicio(exercicio2);
 
         treino1.ExecutarTreino();
     }
